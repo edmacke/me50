68641e0e2ebd885fa219a3d9a20c45eee1a61a22
Ed Macke <edmacke@gmail.com>
2025-08-15 14:11:25 -0500

    Bank working

---
+++ bank/bank.py
@@ -0,0 +1,25 @@
+# In a file called bank.py, implement a program that prompts the user for a greeting.
+# If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20.
+# Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+
+
+def main():
+    msg = input('Enter a greeting: ')
+
+    print(answer(msg))
+
+
+def answer(msg):
+    word = msg.strip().lower()
+
+    if word.startswith('hello'):
+        return '$0'
+
+    if word.startswith('h'):
+        return '$20'
+
+    return '$100'
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_week1.py
+++ tests/test_week1.py
@@ -1,22 +1,45 @@
+import bank.bank as bank
 import deep.deep as deep
+
 
 def test_42():
     assert 'Yes' == deep.answer('42')
 
+
 def test_42():
     assert 'Yes' == deep.answer(' 42 ')
+
 
 def test_42_words_hyphen():
     assert 'Yes' == deep.answer('forty-two')
 
+
 def test_42_words_space():
     assert 'Yes' == deep.answer('forty two')
+
 
 def test_42_words_case():
     assert 'Yes' == deep.answer('FORTY TWO')
     assert 'Yes' == deep.answer('FORTY-TWO')
 
+
 def test_43_words_case():
     assert 'No' == deep.answer('43')
 
 
+def test_bank_hello_leading_space():
+    assert '$0' == bank.answer('   Hello')
+
+
+def test_bank_hello_trailing_space():
+    assert '$0' == bank.answer('Hello ')
+
+def test_bank_hello_newman():
+    assert '$0' == bank.answer('Hello, Newman')
+
+def test_bank_hey():
+    assert '$20' == bank.answer('   Hey')
+
+
+def test_bank_bingo():
+    assert '$100' == bank.answer('   What''s up')
